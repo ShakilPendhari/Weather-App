efff2b5f3d879b99e9dc5b2bb806af8cb7509581
Shakil <shakilpp123456@gmail.com>
2025-03-04 05:19:00 +0530

    fix: change the logic and then try

--- src/app/Components/ServiceWorker.js
+++ src/app/Components/ServiceWorker.js
@@ -1,17 +1,18 @@
-'use client';
+"use client";
 
-import { useEffect } from 'react';
+import { useEffect } from "react";
 
 export default function ServiceWorker() {
   useEffect(() => {
-    if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+    // if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+    if ("serviceWorker" in navigator) {
       navigator.serviceWorker
-        .register('/sw.js')
+        .register("/sw.js")
         .then((registration) => {
-          console.log('SW registered:', registration);
+          console.log("SW registered:", registration);
         })
         .catch((error) => {
-          console.log('SW registration failed:', error);
+          console.log("SW registration failed:", error);
         });
     }
   }, []);
